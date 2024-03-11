8e0f91de883c843a5d9a7c713cc7ad371f277331
lokesh0903sh <lokesh0903sh@gmail.com>
2024-03-12 00:29:09 +0530

    New files

---
+++ app.js
@@ -0,0 +1,19 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    inp.value = "";
+});
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+})
